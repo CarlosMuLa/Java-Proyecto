fd3d657326c3352c163b281e048e0d79c2b7b56b
CarlosMuLa <carlos.muniz@iteso.mx>
2023-11-24 19:27:12 -0600

    Avion
    class avion

--- src/aerolinea/Avion.java
+++ src/aerolinea/Avion.java
@@ -2,23 +2,95 @@ package aerolinea;
 
 public class Avion 
 {
-	public int modeloint;
-	public String modelostr;
-	public int capaciad;
-	public float velocidad;
+	private String modelostr;
+	private int capacidad;
+	private float velocidad;
 
 	
 	String modelosA[] = {"Boeing 737","Airbus A320","Boeing 777","Airbus A330","Boeing 747"};
-	public Avion (int model, int capacidad, float velocidad)
+	public Avion (int model)
 	{
-		
+		setModel(model);
+		setCapacidad(model);
+		setVelocidad(model);
 	}
 	
 	private void setModel(int i)
 	{
-		if (i>=5)
-			i = 4;
+		if (i>=5) {
+			i = 0;
+			modelostr=modelosA[i];
+		}
 		else 
 			modelostr=modelosA[i];
+	}
+	
+	private void setCapacidad(int model)
+	{
+		switch(model)
+		{
+		case 0:
+			capacidad = 149;
+			break;
+		
+		case 1: 
+			capacidad = 240;
+			break;
+		case 2:
+			capacidad = 440;
+			break;
+		case 3:
+			capacidad = 406;
+			break;
+		case 4:
+			capacidad = 660;
+			break;
+		default:
+			capacidad = 149;
+		}
+	}
+	
+	private void setVelocidad(int model)
+	{
+		switch(model)
+		{
+		case 0:
+			velocidad = 900;
+			break;
+		
+		case 1: 
+			velocidad = 900;
+			break;
+		case 2:
+			velocidad = 950;
+			break;
+		case 3:
+			velocidad = 913;
+			break;
+		case 4:
+			velocidad = 950;
+			break;
+		default:
+			velocidad = 900;
+		}	
+	}
+	public String toString()
+	{
+		return "El avion modelo " +modelostr+" tiene una capacidad de "+capacidad+" pasajeros y puede alcanzar una velocidad de " +velocidad+"km/h"; 
+	}
+	
+	public String getModelo()
+	{
+		return modelostr;
+	}
+	
+	public int getCapacidad()
+	{
+		return capacidad;
+	}
+	
+	public float getVelocidad()
+	{
+		return velocidad;
 	}
 }
--- src/aerolinea/FelipeAngeles.java
+++ src/aerolinea/FelipeAngeles.java
@@ -5,7 +5,19 @@ public class FelipeAngeles
 	public static void main(String[]args)
 	{
 		PrimeraClase pc1 = new PrimeraClase("Carlos Muñiz Lara",false,21,"1234");
-		System.out.println(pc1);
+		Avion a1 = new Avion(0);
+		Avion a2 = new Avion(1);
+		Avion a3 = new Avion(2);
+		Avion a4 = new Avion(3);
+		Avion a5 = new Avion(4);
+		Avion a6 = new Avion(5);
+		//System.out.println(pc1);
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
 	}
 
 }
